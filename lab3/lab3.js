ebf299140056321e4e349dd6b8b06e13fe77e479
Vishuvishu <98393037+Vishuvishu@users.noreply.github.com>
2023-01-05 12:58:28 +0530

    Update lab3.js

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -1,60 +1,74 @@
 console.log("connected...")
 
-arr1 = [];
 function getorg(arr1){
+    
+        console.log("getorg")
+document.getElementById("ulid").innerHTML=null;
+for(i in arr1){
+    
+            const ele12 = document.createElement("li")
+            ele12.textContent = arr1[i];
 
-    console.log("getorg")
-
-for(let i=0;i<arr1.length;i++)
-{
-    const ele12 = document.createElement("li")
-    ele12.textContent = arr1;
-
-  
         
-    document.getElementById("ulid").appendChild(ele12);
-
+                
+            document.getElementById("ulid").appendChild(ele12);
+}
 }
 
-}
+
 function getsort(arr1)
 {
     console.log("getsort")
     arr1.sort();
-    for(let i=0;i<arr1.length;i++)
-    {
+   document.getElementById("ulid").innerHTML=null;
+    for(let i in arr1){
+
         const ele12 = document.createElement("li")
-        ele12.textContent = arr1;
-    
-      
+        ele12.textContent = arr1[i];
+  
             
         document.getElementById("ulid").appendChild(ele12);
-    
     }
+   
 }
 
 let arr=[];
-
+let org=[];
 function getfun(){
     console.log("function called");
     
     //const ele = document.getElementById("getid");
-    arr.push(document.getElementById("getid").value);
+   
+    let b=document.getElementById("getid");
 
-    arr.sort();
-
-}
-function change(){
+    arr.push(b.value);
+    org.push(b.value);
 
     const hello = document.getElementById("selid");
+
     if(hello.value=="sort")
     {
         getsort(arr);
     }
     else
     {
-        getorg(arr);
+       
+        getorg(org);
     }
+
+    
+
+}
+
+function change(){
+
+    console.log("just a function");
+    const hello=document.getElementById("selid");
+    if(hello.value=="sort")
+    {
+        getsort(arr);
+    }
+    else getorg(org);
 
 }
    // const ele1 = document.createElement("li")
