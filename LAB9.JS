3730e443c1cf5cf3843eb018be5cb7e934edc4be
Vishuvishu <98393037+Vishuvishu@users.noreply.github.com>
2023-02-23 14:09:58 +0530

    Create LAB9.JS

---
+++ LAB9.JS
@@ -0,0 +1,11 @@
+arr = [1,2,3,45,6,7,8,9,10];
+//FOREAC  //MAP //SOEM //FILER //FIND EVERY //VGSWGH
+
+function foreach_func(){
+    arr.forEach(function(arr){
+        //document.getElementById("HQLLO").appendChild=arr;
+        let X = document.createElement("P")
+        X.innerText=arr;
+        document.body.appendChild(X)
+    })
+}
